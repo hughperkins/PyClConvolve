069c32071ada7284a6352890db6cfc507e29d152
Hugh Perkins <hughperkins@gmail.com>
2015-03-24 23:28:01 +0800

    make net def a bit more complicated :-)

--- test_clconvolve.py
+++ test_clconvolve.py
@@ -1,6 +1,6 @@
 import PyClConvolve
 
-net = PyClConvolve.NeuralNet(3,10)
-PyClConvolve.NetdefToNet.createNetFromNetdef( net, "32c5-10n" ) 
+net = PyClConvolve.NeuralNet(1,28)
+PyClConvolve.NetdefToNet.createNetFromNetdef( net, "rt2-8c5-mp2-16c5-mp3-150n-10n" ) 
 print net.asString()
 
