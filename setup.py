712af101d7052618a0ac328ac7797e65dd090293
Hugh Perkins <hughperkins@gmail.com>
2015-03-28 18:25:55 +0800

    put back warning for python 2.7 on windows

--- setup.py
+++ setup.py
@@ -117,9 +117,9 @@ else:
    pass
    # put other options etc here if necessary
 
-#if osfamily == 'Windows' and sys.version_info[0] == 2:
-#    print('WARNING: python 2.x not really supported, since it needs visual studio 9; and visual studio 2009 doesnt support any c++11 features, which we would ideally prefer to be able to use')
-#    print('Probably possible to coerce ClConvolve to work with visual studio 9, and by extension with python 2.x, but maybe easier to just use python 3.x instead?')
+if osfamily == 'Windows' and sys.version_info[0] == 2:
+    print('WARNING: python 2.x not really supported, since it needs visual studio 9; and visual studio 2009 doesnt support any c++11 features, which we would ideally prefer to be able to use')
+    print('Probably possible to coerce ClConvolve to work with visual studio 9, and by extension with python 2.x, but maybe easier to just use python 3.x instead?')
 
 ext_modules = [
     Extension("libOpenCLHelper",
